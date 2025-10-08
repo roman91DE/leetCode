998ae187fdac737c41d8cffd3f0532d01caebeac
rohoehn <rohoehn123@gmail.com>
2025-10-08 21:35:18 +0200

    first
    Signed-off-by: rohoehn <rohoehn123@gmail.com>

---
+++ dsa/python/add_two_numbers.py
@@ -0,0 +1,55 @@
+from typing import Optional
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+
+        def helper(l1:  Optional[ListNode], l2:  Optional[ListNode], acc=None, carry=False):
+        
+            if (l1 is None) and (l2 is None) and (not carry):
+                return acc
+
+            d = 0
+
+            if l1 is not None:
+                d += l1.val
+                l1 = l1.next
+
+            if l2 is not None:
+                d += l2.val
+                l2 = l2.next
+            
+            if carry:
+                d += 1
+
+            carry = d > 9
+            d = d % 10
+
+            if acc is None:
+                acc = ListNode(val=d)
+            else:
+                p = acc
+                while p.next is not None:
+                    p = p.next
+                else:
+                    p.next = ListNode(val=d)
+        
+            return helper(l1, l2, acc, carry)
+
+        return helper(l1, l2)
+                
+
+        
+
+
+
+
+        
+
